6a00a1b69731255bbec8d4846cc0d96cad312cbd
dormaayan <dor.d.ma@gmail.com>
2019-12-31 22:42:56 +0200

    imports

---
+++ learner.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from math import floor, ceil
+from pylab import rcParams
+
+
+print('hi')
