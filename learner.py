d629291f009371b8e9e8522cd203a1884a7d918f
dormaayan <dor.d.ma@gmail.com>
2020-01-02 22:48:30 +0200

    sync

--- learner.py
+++ learner.py
@@ -7,4 +7,28 @@ from math import floor, ceil
 from pylab import rcParams
 
 
-print('hi')
+def multilayer_perceptron(x, weights, biases, keep_prob):
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(layer_1)
+    layer_1 = tf.nn.dropout(layer_1, keep_prob)
+    out_layer = tf.matmul(layer_1, weights['out']) + biases['out']
+    return out_layer
+
+
+sns.set(style='ticks', palette='Spectral', font_scale=1.5)
+
+material_palette = ["#4CAF50", "#2196F3", "#9E9E9E", "#FF9800", "#607D8B", "#9C27B0"]
+sns.set_palette(material_palette)
+rcParams['figure.figsize'] = 16, 8
+
+plt.xkcd();
+random_state = 42
+np.random.seed(random_state)
+#tf.set_random_seed(random_state)
+
+frame = pd.read_csv("complete-frame.csv", sep=",")
+
+print frame.shape
+
+frame.no_mutations.value_counts().plot(kind="bar", rot=0);
+
