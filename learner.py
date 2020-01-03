1496b33d858f4a7e2bb81cbc2540b2da2b6186ae
dormaayan <dor.d.ma@gmail.com>
2020-01-03 19:46:54 +0200

    sync

--- learner.py
+++ learner.py
@@ -14,6 +14,8 @@ def multilayer_perceptron(x, weights, biases, keep_prob):
     out_layer = tf.matmul(layer_1, weights['out']) + biases['out']
     return out_layer
 
+def encode(series):
+  return pd.get_dummies(series.astype(str))
 
 sns.set(style='ticks', palette='Spectral', font_scale=1.5)
 
@@ -24,11 +26,99 @@ rcParams['figure.figsize'] = 16, 8
 plt.xkcd();
 random_state = 42
 np.random.seed(random_state)
-#tf.set_random_seed(random_state)
 
 frame = pd.read_csv("complete-frame.csv", sep=",")
 
-print frame.shape
+train_x = pd.get_dummies(students_df.school)
+train_x['age'] = students_df.age
+train_x['absences'] = students_df.absences
+train_x['g1'] = students_df.G1
+train_x['g2'] = students_df.G2
+train_x['g3'] = students_df.G3
+train_x = pd.concat([train_x, encode(students_df.sex), encode(students_df.Pstatus),
+                     encode(students_df.Medu), encode(students_df.Fedu),
+                     encode(students_df.guardian), encode(students_df.studytime),
+                     encode(students_df.failures), encode(students_df.activities),
+                     encode(students_df.higher), encode(students_df.romantic),
+                     encode(students_df.reason), encode(students_df.paid),
+                     encode(students_df.goout), encode(students_df.health),
+                     encode(students_df.famsize), encode(students_df.course)
+                    ], axis=1)
 
-frame.no_mutations.value_counts().plot(kind="bar", rot=0);
+train_y = encode(students_df.drinker)
 
+#mutation	no_mutations	line_coverage	isAssertionRoulette	isEagerTest	isLazyTest	isMysteryGuest	isSensitiveEquality	isResourceOptimism	isForTestersOnly	isIndirectTesting	LOC_prod	HALSTEAD_prod	RFC_prod	CBO_prod	MPC_prod	IFC_prod	DAC_prod	DAC2_prod	LCOM1_prod	LCOM2_prod	LCOM3_prod	LCOM4_prod	CONNECTIVITY_prod	LCOM5_prod	COH_prod	TCC_prod	LCC_prod	ICH_prod	WMC_prod	NOA_prod	NOPA_prod	NOP_prod	McCABE_prod	BUSWEIMER_prod	LOC_test	HALSTEAD_test	RFC_test	CBO_test	MPC_test	IFC_test	DAC_test	DAC2_test	LCOM1_test	LCOM2_test	LCOM3_test	LCOM4_test	CONNECTIVITY_test	LCOM5_test	COH_test	TCC_test	LCC_test	ICH_test	WMC_test	NOA_test	NOPA_test	NOP_test	McCABE_test	BUSWEIMER_test	csm_CDSBP	csm_CC	csm_FD	csm_Blob	csm_SC	csm_MC	csm_LM	csm_FE	prod_readability	test_readability	Mutation_Rank
+#Data Exploration
+
+#todo: add here in case needed
+
+
+#Splitting the data
+
+train_size = 0.9
+
+train_cnt = floor(train_x.shape[0] * train_size)
+x_train = train_x.iloc[0:train_cnt].values
+y_train = train_y.iloc[0:train_cnt].values
+x_test = train_x.iloc[train_cnt:].values
+y_test = train_y.iloc[train_cnt:].values
+
+#NN Structure
+n_hidden_1 = 38
+n_input = train_x.shape[1]
+n_classes = train_y.shape[1]
+
+weights = {
+    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'out': tf.Variable(tf.random_normal([n_hidden_1, n_classes]))
+}
+
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+keep_prob = tf.placeholder("float")
+
+training_epochs = 5000
+display_step = 1000
+batch_size = 32
+
+x = tf.placeholder("float", [None, n_input])
+y = tf.placeholder("float", [None, n_classes])
+
+
+#Training
+
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=predictions, labels=y))
+
+optimizer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
+
+predictions = multilayer_perceptron(x, weights, biases, keep_prob)
+
+
+#Evaluation
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for epoch in range(training_epochs):
+        avg_cost = 0.0
+        total_batch = int(len(x_train) / batch_size)
+        x_batches = np.array_split(x_train, total_batch)
+        y_batches = np.array_split(y_train, total_batch)
+        for i in range(total_batch):
+            batch_x, batch_y = x_batches[i], y_batches[i]
+            _, c = sess.run([optimizer, cost],
+                            feed_dict={
+                                x: batch_x,
+                                y: batch_y,
+                                keep_prob: 0.8
+                            })
+            avg_cost += c / total_batch
+        if epoch % display_step == 0:
+            print("Epoch:", '%04d' % (epoch + 1), "cost=", \
+                  "{:.9f}".format(avg_cost))
+    print("Optimization Finished!")
+    correct_prediction = tf.equal(tf.argmax(predictions, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print("Accuracy:", accuracy.eval({x: x_test, y: y_test, keep_prob: 1.0}))
