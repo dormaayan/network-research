2cc418c72563fa05d3066fe4ee714074f1afcb1d
dormaayn <dor.d.ma@gmail.com>
2020-05-24 08:06:15 +0300

    sync

--- evaluation/runner.py
+++ evaluation/runner.py
@@ -1,11 +1,11 @@
 import randomGrid
 
 def main():
-	str = ''
-	for i in range(0,2):
-		s = randomGrid.simpleGrid(consider_coverage=False)
-		str = str + '\n'
-	print(str)
+	#str = ''
+	#for i in range(0,2):
+	s = randomGrid.simpleGrid(consider_coverage=False)
+	#	str = str + '\n'
+	#print(str)
 
 
 if __name__ == '__main__':
