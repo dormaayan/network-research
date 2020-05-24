962710e70ebf61303645033fe5bcc9ed01a3c92a
dormaayn <dor.d.ma@gmail.com>
2020-05-24 08:14:08 +0300

    sync

--- evaluation/runner.py
+++ evaluation/runner.py
@@ -1,9 +1,14 @@
 import randomGrid
+import csv   
+
 
 def main():
 	#str = ''
 	#for i in range(0,2):
 	s = randomGrid.simpleGrid(consider_coverage=False)
+	with open(r'res.csv', 'a') as f:
+	    writer = csv.writer(f)
+	    writer.writerow(s)
 	#	str = str + '\n'
 	#print(str)
 
